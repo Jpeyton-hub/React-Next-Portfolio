ec66b0383f1eb97c5088bb9186202024375d7465
Jpeyton <jpwalk319@gmail.com>
2021-07-07 09:35:44 -0600

    added navbar and links along with the pages for those links

--- components/header.js
+++ components/header.js
@@ -1,11 +1,51 @@
-
+import Image from "next/image";
+import Link from "next/link";
 
 const Header = () => {
-    return (
-        <header>
-            
-        </header>
-    )
-}
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">
+          <Image src="/LogoBlack.png" height={50} width={50} />
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link href='/'><a className="nav-link active" aria-current="page" href="#">
+                Home
+              </a></Link>
+            </li>
+            <li className="nav-item">
+            <Link href='/'><a className="nav-link" href="#">
+                Projects
+              </a></Link>
+            </li>
+            <li className="nav-item">
+            <Link href='/'><a className="nav-link" href="#">
+                About
+              </a></Link>
+            </li>
+            <li className="nav-item">
+            <Link href='/'><a className="nav-link" href="#">
+                Contact
+              </a></Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
 
-export default Header
+export default Header;
--- components/layout.js
+++ components/layout.js
@@ -1,10 +1,18 @@
 import Head from "next/head";
+import Header from "./header";
+import Footer from "./footer";
 
 const Layout = ({ children }) => {
   return (
     <>
       <Head>
-        <link rel="icon" href="/LogoBlack" />
+        <link
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+          integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
+          crossOrigin="anonymous"
+        />
+        <link rel="icon" href="/LogoBlack.png" />
         <meta
           name="description"
           content="Personal portfolio made with Next.js!"
@@ -13,10 +21,9 @@ const Layout = ({ children }) => {
       <Header />
       <main>{children}</main>
       <Footer />
-
       <script
-        src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4"
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
         crossOrigin="anonymous"
       ></script>
     </>
