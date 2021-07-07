d62156942630ab0b359bf85bb459ce914b5492da
Jpeyton <jpwalk319@gmail.com>
2021-07-07 08:58:43 -0600

    built basic layout component to appear in all pages.

--- components/footer.js
+++ components/footer.js
@@ -2,9 +2,9 @@
 
 const Footer = () => {
     return (
-        <div>
+        <footer>
             
-        </div>
+        </footer>
     )
 }
 
--- components/layout.js
+++ components/layout.js
@@ -1,11 +1,26 @@
+import Head from "next/head";
 
+const Layout = ({ children }) => {
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/LogoBlack" />
+        <meta
+          name="description"
+          content="Personal portfolio made with Next.js!"
+        />
+      </Head>
+      <Header />
+      <main>{children}</main>
+      <Footer />
 
-const Layout = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+      <script
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4"
+        crossOrigin="anonymous"
+      ></script>
+    </>
+  );
+};
 
-export default Layout
+export default Layout;
